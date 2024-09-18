e2814013d4fecf92d23cf19f2fd52f010bac9014
abdullah1261 <l201261@lhr.nu.edu.pk>
2024-09-19 02:19:08 +0500

    Message Component, chat and Attachment

--- client/src/constants/color.js
+++ client/src/constants/color.js
@@ -2,3 +2,4 @@ export const orange = "#ea7070";
 export const blue = "#1e90ff";
 export const neon = "#16F529";
 export const grayColor = "rgba(247,247,247,1)";
+export const lightblue = "#2694ab";
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useRef } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import AppLayout from "../components/layout/AppLayout";
 import { IconButton, Stack } from "@mui/material";
 import { grayColor } from "../constants/color";
@@ -8,10 +8,26 @@ import {
   Send as SendIcon,
 } from "@mui/icons-material";
 import { blue } from "../constants/color";
-import {FileMenu} from "../dialogs/FileMenu";
+import FileMenu from "../components/dialogs/FileMenu";
+import { sampleMessage } from "../constants/sampleData";
+import MessageComponent from "../components/shared/MessageComponent";
+
+const user = {
+  _id: "kscchkdk",
+  name: "Abdullah",
+};
 
 const Chat = () => {
   const containerRef = useRef(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget); // Opens the FileMenu
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null); // Closes the FileMenu
+  };
 
   return (
     <Fragment>
@@ -27,7 +43,9 @@ const Chat = () => {
           overflowY: "auto",
         }}
       >
-        {}
+        {sampleMessage.map((i) => (
+          <MessageComponent key={i._id} message={i} user={user} />
+        ))}
       </Stack>
       <form
         style={{
@@ -41,29 +59,37 @@ const Chat = () => {
           alignItems={"center"}
           position={"relative"}
         >
-          <IconButton sx={{
-            position:"absolute",
-            left: "1.5rem",
-
-
-          }}>
+          <IconButton
+            sx={{
+              position: "absolute",
+              left: "1.5rem",
+            }}
+            onClick={handleMenuOpen} // Opens the FileMenu when clicked
+          >
             <AttachFileIcon />
           </IconButton>
-          <InputBox placeholder="Type Message Here..."/>
-          <IconButton type="submit" sx={{
-            backgroundColor: blue,
-            color:"white",
-            marginLeft:"1rem",
-            padding:"0.5rem",
-            "&:hover":{
-              backgroundColor:"#1565C0",
-            }
-          }}>
+          <InputBox placeholder="Type Message Here..." />
+          <IconButton
+            type="submit"
+            sx={{
+              backgroundColor: blue,
+              color: "white",
+              marginLeft: "1rem",
+              padding: "0.5rem",
+              "&:hover": {
+                backgroundColor: "#1565C0",
+              },
+            }}
+          >
             <SendIcon />
           </IconButton>
         </Stack>
       </form>
-      <FileMenu/>
+      <FileMenu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)} // Controls the visibility of the FileMenu
+        onClose={handleMenuClose}
+      />
     </Fragment>
   );
 };
